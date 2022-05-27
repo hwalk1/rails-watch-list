0011634537372eb9211e66c45e56b67b9e11055f
hayden walker <haydenjwalker1@gmail.com>
2022-05-27 15:15:17 +1000

    Root updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get "lists", to: "lists#index"
   # get "lists/:id", to: "lists#show"
-
-  resources :lists, only: [:index, :show, :new, :create] do
+  root to: 'lists#index'
+  resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [ :destroy ]
